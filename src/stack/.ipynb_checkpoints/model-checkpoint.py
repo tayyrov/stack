755bb32f912ec9fb97c0c4e79ffd77a9dd7482e5
MingzeDong <mingze.dong@yale.edu>
2026-01-10 02:57:35 -0800

    remove .ipynb_checkpoints

--- src/stack/.ipynb_checkpoints/model-checkpoint.py
+++
@@ -1,8 +0,0 @@
-"""Backward compatible entry-point for the core StateICL model."""
-
-from __future__ import annotations
-
-from .models.core import StateICLModel, scShiftAttentionModel
-from .model_loading import load_model_from_checkpoint
-
-__all__ = ["StateICLModel", "scShiftAttentionModel", "load_model_from_checkpoint"]
